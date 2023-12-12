ef8678c21abbe6aa65fd03de32b3987db2c90b3d
nm1008 <mallari08nikko@yahoo.com>
2023-12-12 23:58:56 +0800

    implemented calculator buttons

--- src/pages/Calculator/Calculator.jsx
+++ src/pages/Calculator/Calculator.jsx
@@ -1,27 +1,72 @@
 const Calculator = () => {
   return (
-    <div className="flex gap-5 flex-column bg-red">
-      <button>Clear</button>
-      <button>C</button>
-      <button>/</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>*</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>-</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>+</button>
-      <button>0</button>
-      <button>.</button>
-      <button>=</button>
-      
+    <div className="container mx-auto flex flex-col justify-center mt-20 bg-amber-400 p-4 w-1/4">
+      <div className="grid grid-cols-4 ">
+        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
+          7
+        </button>
+        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
+          8
+        </button>
+        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
+          9
+        </button>
+        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
+          Del
+        </button>
+        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
+          4
+        </button>
+        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
+          5
+        </button>
+        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
+          6
+        </button>
+        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
+          +
+        </button>
+        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
+          1
+        </button>
+        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
+          2
+        </button>
+        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
+          3
+        </button>
+        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
+          -
+        </button>
+        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
+          .
+        </button>
+        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
+          0
+        </button>
+        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
+          /
+        </button>
+        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
+          X
+        </button>
+      </div>
+      <div className="flex">
+        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold grow">
+          Reset
+        </button>
+        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold grow ">
+          =
+        </button>
+      </div>
     </div>
   );
 };
 
 export default Calculator;
+
+{
+  /* 
+       
+      </div> */
+}
