aae92c2cf1926594dfac8fec845a2fc605657dc2
nm1008 <mallari08nikko@yahoo.com>
2023-12-13 03:11:46 +0800

    added functionalities for calculator

--- src/pages/Calculator/Calculator.jsx
+++ src/pages/Calculator/Calculator.jsx
@@ -1,72 +1,157 @@
+import { useState } from "react";
+
 const Calculator = () => {
+  const [input, setInput] = useState("0");
+  const [result, setResult] = useState("");
+
+  const handleButtonClick = (value) => {
+    if (input === "0") {
+      setInput(value);
+    } else {
+      setInput((prevInput) => prevInput + value);
+    }
+  };
+
+  const handleClear = () => {
+    setInput("0");
+    setResult("");
+  };
+
+  const handleCalculate = () => {
+    try {
+      setResult(eval(input).toString());
+    } catch (error) {
+      setResult("Error");
+    }
+  };
+
+  const handleDelete = () => {
+    setInput(input.slice(0, -1))
+  }
+
   return (
-    <div className="container mx-auto flex flex-col justify-center mt-20 bg-amber-400 p-4 w-1/4">
-      <div className="grid grid-cols-4 ">
-        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
-          7
-        </button>
-        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
-          8
-        </button>
-        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
-          9
-        </button>
-        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
-          Del
-        </button>
-        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
-          4
-        </button>
-        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
-          5
-        </button>
-        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
-          6
-        </button>
-        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
-          +
-        </button>
-        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
-          1
-        </button>
-        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
-          2
-        </button>
-        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
-          3
-        </button>
-        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
-          -
-        </button>
-        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
-          .
-        </button>
-        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
-          0
-        </button>
-        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
-          /
-        </button>
-        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold ">
-          X
-        </button>
+    <section className="container mx-auto flex flex-col justify-center mt-10 w-full  lg:w-1/3  md:w-1/2 ">
+      <div className=" bg-blue-500 p-5 mx-5 rounded-lg">
+        <div className="flex flex-col bg-white rounded-lg mx-2 my-5 p-5">
+          <h1 className="grow text-end font-bold text-lg ">
+           {input}
+          </h1>
+          <h1 className="grow text-end font-bold text-lg ">{result}</h1>
+        </div>
+        <div className="grid grid-cols-4 ">
+          <button
+            className="bg-white mx-2 my-3 p-2w-auto rounded-lg text-md font-bold md:text-lg "
+            onClick={() => handleButtonClick("7")}
+          >
+            7
+          </button>
+          <button
+            className="bg-white mx-2 my-3 p-5 rounded-lg text-md font-bold md:text-lg"
+            onClick={() => handleButtonClick("8")}
+          >
+            8
+          </button>
+          <button
+            className="bg-white mx-2 my-3 p-5 rounded-lg text-md font-bold md:text-lg "
+            onClick={() => handleButtonClick("9")}
+          >
+            9
+          </button>
+          <button className="bg-white mx-2 my-3 p-5 rounded-lg text-md font-bold md:text-lg "
+            onClick={handleDelete}
+          >
+            Del
+          </button>
+          <button
+            className="bg-white mx-2 my-3 p-5 rounded-lg text-md font-bold md:text-lg "
+            onClick={() => handleButtonClick("4")}
+          >
+            4
+          </button>
+          <button
+            className="bg-white mx-2 my-3 p-5 rounded-lg text-md font-bold md:text-lg "
+            onClick={() => handleButtonClick("5")}
+          >
+            5
+          </button>
+          <button
+            className="bg-white mx-2 my-3 p-5 rounded-lg text-md font-bold md:text-lg "
+            onClick={() => handleButtonClick("6")}
+          >
+            6
+          </button>
+          <button
+            className="bg-white mx-2 my-3 p-5 rounded-lg text-md font-bold md:text-lg "
+            onClick={() => handleButtonClick("+")}
+          >
+            +
+          </button>
+          <button
+            className="bg-white mx-2 my-3 p-5 rounded-lg text-md font-bold md:text-lg "
+            onClick={() => handleButtonClick("1")}
+          >
+            1
+          </button>
+          <button
+            className="bg-white mx-2 my-3 p-5 rounded-lg text-md font-bold md:text-lg "
+            onClick={() => handleButtonClick("2")}
+          >
+            2
+          </button>
+          <button
+            className="bg-white mx-2 my-3 p-5 rounded-lg text-md font-bold md:text-lg "
+            onClick={() => handleButtonClick("3")}
+          >
+            3
+          </button>
+          <button
+            className="bg-white mx-2 my-3 p-5 rounded-lg text-md font-bold md:text-lg "
+            onClick={() => handleButtonClick("7")}
+          >
+            -
+          </button>
+          <button
+            className="bg-white mx-2 my-3 p-5 rounded-lg text-md font-bold md:text-lg "
+            onClick={() => handleButtonClick("-")}
+          >
+            .
+          </button>
+          <button
+            className="bg-white mx-2 my-3 p-5 rounded-lg text-md font-bold md:text-lg "
+            onClick={() => handleButtonClick("0")}
+          >
+            0
+          </button>
+          <button
+            className="bg-white mx-2 my-3 p-5 rounded-lg text-md font-bold md:text-lg "
+            onClick={() => handleButtonClick("/")}
+          >
+            /
+          </button>
+          <button
+            className="bg-white mx-2 my-3 p-5 rounded-lg text-md font-bold md:text-lg "
+            onClick={() => handleButtonClick("*")}
+          >
+            X
+          </button>
+        </div>
+        <div className="flex">
+          <button
+            className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold grow"
+            onClick={handleClear}
+          >
+            Reset
+          </button>
+          <button
+            className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold grow"
+            onClick={handleCalculate}
+          >
+            =
+          </button>
+        </div>
       </div>
-      <div className="flex">
-        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold grow">
-          Reset
-        </button>
-        <button className="bg-white mx-2 my-3 p-5 rounded-lg text-lg font-bold grow ">
-          =
-        </button>
-      </div>
-    </div>
+    </section>
   );
 };
 
 export default Calculator;
-
-{
-  /* 
-       
-      </div> */
-}
