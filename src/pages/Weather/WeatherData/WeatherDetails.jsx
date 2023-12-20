3f995bc1513098eb14df28a2c27c65ba36273451
nm1008 <mallari08nikko@yahoo.com>
2023-12-21 04:54:55 +0800

    added logo for top bar / fixed ui in weather details

--- src/pages/Weather/WeatherData/WeatherDetails.jsx
+++ src/pages/Weather/WeatherData/WeatherDetails.jsx
@@ -35,13 +35,12 @@ const WeatherDetails = ({ data }) => {
   }, [timeAm, timePm]);
 
   return (
-    <div className="bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-lg xl:p-0">
+    <div className="w-full p-3 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-lg xl:p-0">
       <div className=" space-y-4 h md:space-y-6  sm:p-4">
         <div className="flex flex-col">
           <div className="flex gap-5 mb-5 text-center p-2 ">
             <Card title={"Temps °C (High / Low)"}>
-              {data?.main?.temp_max.toFixed(1)}°C/
-              {data?.main?.temp_min.toFixed(1)}°C
+              {data?.main?.temp_max.toFixed(1)}°C / {data?.main?.temp_min.toFixed(1)}°C
             </Card>
             <Card title={"Humidity / hPA"}>
               {data?.main?.humidity}% / {data?.main?.pressure}
@@ -50,7 +49,7 @@ const WeatherDetails = ({ data }) => {
           <div className="flex gap-5 mb-5  text-center">
             <Card title={"Wind speed"}>{data?.wind?.speed} m/s</Card>
             <Card title={"Sunrise / Sunset"}>
-              {sunrise}AM/{sunset}PM
+              {sunrise}AM / {sunset}PM
             </Card>
           </div>
         </div>
